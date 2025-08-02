89bbff237f6ceee0478d18df9c0b45181697b964
Witnesscm <fanmicao@126.com>
2025-08-02 23:32:56 +0800

    chore: cleanup

--- ItemLevel.lua
+++ ItemLevel.lua
@@ -248,11 +248,10 @@ hooksecurefunc("SetItemButtonQuality", function(self, quality, itemIDOrLink, sup
     end
 end)
 
--- Bag & Bank
+-- Bag
 local function SetContainerItemLevel(self)
-    local category = self.IsBankBag and self:IsBankBag() and "Bank" or "Bag" -- TODO: 11.2.0
     for _, button in self:EnumerateValidItems() do
-        SetItemLevel(button, C_Container.GetContainerItemLink(button:GetBagID(), button:GetID()), category)
+        SetItemLevel(button, C_Container.GetContainerItemLink(button:GetBagID(), button:GetID()), "Bag")
     end
 end
 
@@ -267,15 +266,7 @@ if ContainerFrameCombinedBags then
     hooksecurefunc(ContainerFrameCombinedBags, "UpdateItems", SetContainerItemLevel)
 end
 
--- Bank -- TODO: 11.2.0
-if BankFrameItemButton_Update then
-    hooksecurefunc("BankFrameItemButton_Update", function(self)
-        if self.isBag then return end
-        SetItemLevel(self, C_Container.GetContainerItemLink(self:GetParent():GetID(), self:GetID()), "Bank")
-    end)
-end
-
--- Warband Bank -- TODO: 11.2.0
+-- Bank
 if BankPanelItemButtonMixin then
     hooksecurefunc(BankPanelItemButtonMixin, "Refresh", function(self)
         SetItemLevel(self, self.itemInfo and self.itemInfo.hyperlink, "Bank")
