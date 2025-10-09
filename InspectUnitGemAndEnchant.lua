c2909ba8024a6b606b55d9c4e2e9454d91a6f993
Witnesscm <fanmicao@126.com>
2025-10-09 20:25:52 +0800

    feat: hide enchant missing icon in Remix

--- InspectUnitGemAndEnchant.lua
+++ InspectUnitGemAndEnchant.lua
@@ -248,7 +248,7 @@ local function ShowGemAndEnchant(frame, ItemLink, anchorFrame, itemframe)
         icon:SetPoint("LEFT", anchorFrame, "RIGHT", num == 1 and 6 or 1, 0)
         icon:Show()
         anchorFrame = icon
-    elseif (not enchantID and CheckEnchantmentSlot(itemframe.index, itemframe.quality, itemframe.classID)) then
+    elseif (not enchantID and CheckEnchantmentSlot(itemframe.index, itemframe.quality, itemframe.classID) and not PlayerIsTimerunning()) then
         num = num + 1
         icon = GetIconFrame(frame)
         icon.title = ENCHANTS..": "..itemframe.slot
