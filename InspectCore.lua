85c3f8f6b7e342d0623669035332e38707dc5b24
xuhuan <xuhuanxxx@gmail.com>
2026-01-25 02:57:41 +0800

    feat: Refactor tooltip API and add progressive loading for equipment list
    feat: Tooltip API重构与装备列表渐进加载
    - 使用 tooltipData.lines 替代遗留全局对象访问
    - 打开检查窗口时立即显示框架，支持缓存秒加载

--- InspectCore.lua
+++ InspectCore.lua
@@ -118,9 +118,9 @@ LibEvent:attachEvent("INSPECT_READY", function(this, guid)
     LibSchedule:AddTask({
         identity  = guid,
         timer     = 0.1,
-        elasped   = 0.4,
+        elasped   = 0,
         expired   = GetTime() + 5,
-        repeats   = 2,  --重复次数 10.x里GetInventoryItemLink居然有概率返回nil,所以这里扫两次
+        repeats   = 1,
         data      = guids[guid],
         onTimeout = function(self) inspecting = false end,
         onExecute = function(self)
--- InspectMouse.lua
+++ InspectMouse.lua
@@ -4,79 +4,83 @@
 -------------------------------------
 local addon, ns = ...
 
+if ns.IsMidnight then return end
+
 local LibEvent = LibStub:GetLibrary("LibEvent.7000")
 
-local function FindLine(tooltip, keyword)
-    local line, text
-    for i = 2, tooltip:NumLines() do
-        line = _G[tooltip:GetName() .. "TextLeft" .. i]
-        if (not line) then break end
-        text = line:GetText() or ""
-        if (string.find(text, keyword)) then
-            return line, i, _G[tooltip:GetName() .. "TextRight" .. i]
+-- 使用 tooltipData.lines 查找文本
+local function FindLineInTooltipData(tooltipData, keyword)
+    if not tooltipData or not tooltipData.lines then return end
+    for i, lineData in ipairs(tooltipData.lines) do
+        if lineData.leftText and string.find(lineData.leftText, keyword) then
+            return i
         end
     end
 end
 
 local LevelLabel = STAT_AVERAGE_ITEM_LEVEL .. ": "
-local SpecLabel = SPECIALIZATION .. ": "
 
-local function AppendToGameTooltip(guid, ilevel, spec, weaponLevel, isArtifact)
+local function AppendToGameTooltip(guid, ilevel, spec, weaponLevel, tooltipData)
     spec = spec or ""
-    if (TinyInspectDB and not TinyInspectDB.EnableMouseSpecialization) then spec = "" end
+    if TinyInspectDB and not TinyInspectDB.EnableMouseSpecialization then spec = "" end
+    
     local _, unit = GameTooltip:GetUnit()
-    if (not unit or UnitGUID(unit) ~= guid) then return end
-    local ilvlLine, _, lineRight = FindLine(GameTooltip, LevelLabel)
+    if not unit or UnitGUID(unit) ~= guid then return end
+    
     local ilvlText = format("%s|cffffffff%s|r", LevelLabel, ilevel)
     local specText = format("|cffb8b8b8%s|r", spec)
-    if (weaponLevel and weaponLevel > 0 and TinyInspectDB.EnableMouseWeaponLevel) then
+    if weaponLevel and weaponLevel > 0 and TinyInspectDB.EnableMouseWeaponLevel then
         ilvlText = ilvlText .. format(" (%s)", weaponLevel)
     end
-    if (ilvlLine) then
-        ilvlLine:SetText(ilvlText)
-        lineRight:SetText(specText)
+    
+    local lineIndex = tooltipData and FindLineInTooltipData(tooltipData, LevelLabel)
+    if lineIndex then
+        local leftLine = _G[GameTooltip:GetName() .. "TextLeft" .. lineIndex]
+        local rightLine = _G[GameTooltip:GetName() .. "TextRight" .. lineIndex]
+        if leftLine then leftLine:SetText(ilvlText) end
+        if rightLine then rightLine:SetText(specText) end
     else
         GameTooltip:AddDoubleLine(ilvlText, specText)
     end
     GameTooltip:Show()
 end
 
---觸發觀察
-if (GameTooltip.ProcessInfo) then
+-- 觸發觀察
+if GameTooltip.ProcessInfo then
     hooksecurefunc(GameTooltip, "ProcessInfo", function(self, info)
-        if (not info or not info.tooltipData) then return end
-        local flag = info.tooltipData.type
-        local guid = info.tooltipData.guid
-        if (flag ~= 2) then return end
+        if not info or not info.tooltipData then return end
+        local tooltipData = info.tooltipData
+        if tooltipData.type ~= 2 then return end
 
-        if (TinyInspectDB and (TinyInspectDB.EnableMouseItemLevel or TinyInspectDB.EnableMouseSpecialization)) then
+        if TinyInspectDB and (TinyInspectDB.EnableMouseItemLevel or TinyInspectDB.EnableMouseSpecialization) then
             local _, unit = self:GetUnit()
-            if (not unit) then return end
+            if not unit then return end
             local hp = UnitHealthMax(unit)
             local data = GetInspectInfo(unit)
-            if (data and data.hp == hp and data.ilevel > 0) then
-                return AppendToGameTooltip(guid, floor(data.ilevel), data.spec, data.weaponLevel, data.isArtifact)
+            if data and data.hp == hp and data.ilevel > 0 then
+                return AppendToGameTooltip(tooltipData.guid, floor(data.ilevel), data.spec, data.weaponLevel, tooltipData)
             end
-            if (not CanInspect(unit) or not UnitIsVisible(unit)) then return end
+            if not CanInspect(unit) or not UnitIsVisible(unit) then return end
             local inspecting = GetInspecting()
-            if (inspecting) then
-                if (inspecting.guid ~= guid) then
-                    return AppendToGameTooltip(guid, "n/a")
+            if inspecting then
+                if inspecting.guid ~= tooltipData.guid then
+                    return AppendToGameTooltip(tooltipData.guid, "n/a", nil, nil, tooltipData)
                 else
-                    return AppendToGameTooltip(guid, "......")
+                    return AppendToGameTooltip(tooltipData.guid, "......", nil, nil, tooltipData)
                 end
             end
             ClearInspectPlayer()
             NotifyInspect(unit)
-            AppendToGameTooltip(guid, "...")
+            AppendToGameTooltip(tooltipData.guid, "...", nil, nil, tooltipData)
         end
     end)
 end
 
---@see InspectCore.lua
+-- @see InspectCore.lua
 LibEvent:attachTrigger("UNIT_INSPECT_READY", function(self, data)
-    if (TinyInspectDB and not TinyInspectDB.EnableMouseItemLevel) then return end
-    if (data.guid == UnitGUID("mouseover")) then
-        AppendToGameTooltip(data.guid, floor(data.ilevel), data.spec, data.weaponLevel, data.isArtifact)
+    if TinyInspectDB and not TinyInspectDB.EnableMouseItemLevel then return end
+    if data.guid == UnitGUID("mouseover") then
+        local tooltipData = C_TooltipInfo.GetUnit("mouseover")
+        AppendToGameTooltip(data.guid, floor(data.ilevel), data.spec, data.weaponLevel, tooltipData)
     end
 end)
